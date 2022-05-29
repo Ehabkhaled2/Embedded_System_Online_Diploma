a1a7205965412123f67cb7d937a607783048dac1
Ehabkhaled22 <ehabkhaled7422@gmail.com>
2022-05-29 22:32:12 +0200

    EX1

---
+++ C_Programming/Assignments/U3L3/Home_Work_1/EX_1/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ *  Created on: May 29, 2022
+ *      Author: Ehab Khaled
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	//Write C Program to Print a Sentence
+
+	printf("C Programming");
+
+}
