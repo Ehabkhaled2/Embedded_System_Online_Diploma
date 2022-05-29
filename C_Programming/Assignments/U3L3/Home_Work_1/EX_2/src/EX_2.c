74712a8fcfa0f4f6036846562e53f3cf6dfb7fb8
Ehabkhaled22 <ehabkhaled7422@gmail.com>
2022-05-29 22:43:15 +0200

    EX1

---
+++ C_Programming/Assignments/U3L3/Home_Work_1/EX_2/src/EX_2.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : EX_2.c
+ Author      : Ehab Khaled
+
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+	int x=0;
+	printf("Enter an integer: ");
+	fflush(stdout);
+	fflush(stdin);
+	scanf("%i",&x);
+	printf("You entered: %i",x);
+
+}
